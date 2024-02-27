44354c61be871d0d66472ed5418d9e5dff517288
Derek Gray <105393523+DerekGray2022@users.noreply.github.com>
2024-02-27 13:50:06 +0000

    Chapter 3 - Toggle Actions

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ gsap.registerPlugin(ScrollTrigger);
 const square1 = document.querySelector(".square1");
 
 gsap.to('.square1', {
-    // x: 700,
-    duration: 3,
+    x: 200,
+    duration: 8,
     scrollTrigger: {
         trigger: '.square1',
-        start: "top 50%",
-        // end: "center 20%",
-        end: () => `+=${square1.offsetHeight}`,
-        // markers: {
-        //     startColor: 'purple',
-        //     endColor: 'fuchsia',
-        //     fontSize: '1.2rem',
-        //     indent: 30
-        // }
-        markers: true,
-        toggleClass: "red"
+        start: "top 60%",
+        end: "top 40%",
+        //      none | play | restart | reset | reverse | resume | pause | complete
+        toggleActions: "restart pause resume complete",
+        //      onEnter | onLeave | onEnterBack | onLeaveBack
+        markers: {
+            startColor: 'purple',
+            endColor: 'fuchsia',
+            fontSize: '1.2rem',
+            indent: 30
+        },
     }
 });
 
