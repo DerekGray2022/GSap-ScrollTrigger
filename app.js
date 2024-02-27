481af3236fd0879b1bff57838f8827e456b097ac
Derek Gray <105393523+DerekGray2022@users.noreply.github.com>
2024-02-27 12:49:03 +0000

    Chapter 2 - Start & End

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
+const square1 = document.querySelector(".square1");
 
 gsap.to('.square1', {
-    x: 700,
+    // x: 700,
     duration: 3,
-    scrollTrigger: '.square2'
+    scrollTrigger: {
+        trigger: '.square1',
+        start: "top 50%",
+        // end: "center 20%",
+        end: () => `+=${square1.offsetHeight}`,
+        // markers: {
+        //     startColor: 'purple',
+        //     endColor: 'fuchsia',
+        //     fontSize: '1.2rem',
+        //     indent: 30
+        // }
+        markers: true,
+        toggleClass: "red"
+    }
 });
 
 
